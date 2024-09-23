5b4b8996a45a0a2faca3c9ba59fc357e9451abad
Cahyo Wicaksono <cahyo.wicaksono@gmail.com>
2024-09-23 12:15:42 +0700

    initial commmit

---
+++ test/calculatorTest.php
@@ -0,0 +1,23 @@
+<?php
+require 'vendor/autoload.php';
+
+use Calculator\BasicCalculator;
+
+class calculatorTest extends PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Calculator\BasicCalculator
+    */
+    public function testAdd()
+    {
+        $this->assertEquals(BasicCalculator::add(1, 2), 3.0);
+    }
+
+  /**
+  * @covers Calculator\BasicCalculator
+  */
+  public function testSubtract()
+  {
+    $this->assertEquals(BasicCalculator::subtract(1, 2), -1.0);
+  }
+}
